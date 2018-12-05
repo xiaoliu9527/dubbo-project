39657b8345246e6a54d67ad0c4fb46cd6831bea2
wangzl <yi790103177@163.com>
2018-12-05 16:26:00 +0800

    add

--- dubbo-client/src/main/java/com/wzl/study/springboot/dubboclient/DubboClientApplication.java
+++ dubbo-client/src/main/java/com/wzl/study/springboot/dubboclient/DubboClientApplication.java
@@ -3,7 +3,9 @@ package com.wzl.study.springboot.dubboclient;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @EnableDubbo//开启Dubbo
 @SpringBootApplication
 public class DubboClientApplication {
--- dubbo-client/src/main/java/com/wzl/study/springboot/dubboclient/controller/DubboController.java
+++ dubbo-client/src/main/java/com/wzl/study/springboot/dubboclient/controller/DubboController.java
@@ -2,7 +2,10 @@ package com.wzl.study.springboot.dubboclient.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.wzl.study.springboot.service.DubboService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +13,10 @@ public class DubboController {
 
     @Reference(version ="1.0.0",group ="test" )
     private DubboService dubboService;
-    @RequestMapping("/name")
+
+    @ApiOperation(value="测试",notes = "整合springboot集成dubbo")
+    @ApiImplicitParam(name="name",value = "名字",required = true,dataType = "String")
+    @RequestMapping(value = "/name" ,method = RequestMethod.GET)
     public String getName(String name){
         return dubboService.getName(name);
     }
